98ceb7e329c7f352d2983837f7a1b6a965363e96
paulsenm <paulsenm@my.lanecc.edu>
2025-10-15 16:50:00 -0700

    made addr search a function that returns lat and lon

--- addr_search.py
+++ addr_search.py
@@ -1,8 +1,19 @@
 import os
-
+import json
 import requests
 
 API_KEY = os.getenv("GEOCODE_API_KEY")
 
-result = requests.get(f"https://geocode.maps.co/search?q=123 main st, springfield, or &api_key={API_KEY}")
-print(f"result was: {str(result.text)}")
+#result = requests.get(f"https://geocode.maps.co/search?q=123 main st, springfield, or &api_key={API_KEY}")
+#print(f"result was: {str(result.text)}")
+
+def get_lat_lon(address_string):
+    url = "https://geocode.maps.co/search?q=" + address_string + "&api_key=" + API_KEY
+    result = requests.get(url)
+    result_data = result.text
+    result_parsed = json.loads(result_data)
+    print(f"lat was: {result_parsed[0]["lat"]}")
+    lat_lon = [result_parsed[0]["lat"], result_parsed[0]["lon"]]
+    return lat_lon
+
+
